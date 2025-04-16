55caea865ade41d39c6239f79c12fdc2b5eda2c7
Odoemena Kamsy Victor <odoemenakamsy12@gmail.com>
2025-04-16 18:59:04 +0100

    2nd trial on vercel deployment

--- react-native-web.d.ts
+++ react-native-web.d.ts
@@ -1,3 +1,52 @@
 declare module 'react-native-web' {
   export * from 'react-native';
+  
+  // Additional React Native Web specific types can be added here
+  export interface ViewProps {
+    style?: any;
+    [key: string]: any;
+  }
+  
+  export interface TextProps {
+    style?: any;
+    [key: string]: any;
+  }
+  
+  export interface TextInputProps {
+    style?: any;
+    value?: string;
+    onChangeText?: (text: string) => void;
+    placeholder?: string;
+    placeholderTextColor?: string;
+    secureTextEntry?: boolean;
+    keyboardType?: string;
+    [key: string]: any;
+  }
+  
+  export interface TouchableOpacityProps {
+    style?: any;
+    onPress?: () => void;
+    disabled?: boolean;
+    [key: string]: any;
+  }
+  
+  export interface ScrollViewProps {
+    style?: any;
+    [key: string]: any;
+  }
+  
+  export interface StyleSheetStatic {
+    create<T extends StyleSheet.NamedStyles<T> | StyleSheet.NamedStyles<any>>(styles: T): T;
+    flatten(style?: any): any;
+    hairlineWidth: number;
+    absoluteFill: any;
+    absoluteFillObject: any;
+  }
+  
+  export const StyleSheet: StyleSheetStatic;
+  export const View: React.ComponentType<ViewProps>;
+  export const Text: React.ComponentType<TextProps>;
+  export const TextInput: React.ComponentType<TextInputProps>;
+  export const TouchableOpacity: React.ComponentType<TouchableOpacityProps>;
+  export const ScrollView: React.ComponentType<ScrollViewProps>;
 } 
